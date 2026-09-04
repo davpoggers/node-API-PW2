4eaf109008d6d271d68c6b6895a24a75092e07e9
dav_poggers <d4v1dabatata@gmail.com>
2026-09-04 19:52:20 -0300

    Update userController.js

--- SRC/controllers/User/userController.js
+++ SRC/controllers/User/userController.js
@@ -1,6 +1,9 @@
 import express from "express";
+import {AppDataSource} from "../../database/config.js";
+import user from "../../model/user.js";
 
 const route = express.Router();
+const userTable = AppDataSource.getRepository(user);
 
 route.post("/", (request, response) => {
     return response.send("Deu certo yipee!");
@@ -9,6 +12,9 @@ route.post("/", (request, response) => {
 route.post("/", (request, response) => {
     //nome, email, senha, tipo de usuário
     const { name, email, password, typeUser } = request.body;
+
+    const userdata = userTable.create({ name, email, password, typeUser });
+    userTable.save(userdata);
 
     console.log(name, email, password, typeUser);
 });
